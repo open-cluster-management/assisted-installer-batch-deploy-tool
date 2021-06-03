03e5ab8f7cf51619bfbe1283e9764a8b19216dd3
Han Zhang <hanzhang@redhat.com>
2021-06-03 15:59:47 -0400

    update monitoring to add iso & installfailed (#5)
    * update monitoring to add iso & installfailed
    
    Signed-off-by: Hanqiu Zhang <hanzhang@redhat.com>
    
    * update naming
    
    Signed-off-by: Hanqiu Zhang <hanzhang@redhat.com>

--- monitor-deployment.sh
+++ monitor-deployment.sh
@@ -12,9 +12,12 @@ set -o nounset
 
 # The following stats are reported:
 #   - initialized: the number of clusterdeployment that have been created.
+#   - iso_gen: the number of infraenvs that have a discovery iso url generated.
+#   - iso_bmh: the number of baremetalhosts that have been injected with a discovery iso url.
 #   - booted: baremetal hosts that are provisioned; currently running discovery iso and downloading rootfs.
 #   - discovered: rootfs has been downloaded and discovery results sent back to the hub; agent created.
-#   - provisioning: clusterdeployment in provisioning state
+#   - provisioning: agentclusterinstall in provisioning state
+#   - install_failed: agentclusterinstall in failed state
 #   - completed: clusterdeployment in completed state
 #   - managed: managedcluster avaialble
 #   - agents_available: managedclusteraddon avaialble
@@ -33,9 +36,12 @@ if [ ! -f ${file} ]; then
   echo "\
 date,\
 initialized,\
+iso_gen,\
+iso_bmh,\
 booted,\
 discovered,\
 provisioning,\
+install_failed,\
 completed,\
 managed,\
 agents_available\
@@ -45,23 +51,32 @@ fi
 while true; do
   D=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-  clusterdeployments_readyforinstallation_and_installed=$(oc get clusterdeployment -A --no-headers -o custom-columns=READY:'.status.conditions[?(@.type=="ReadyForInstallation")].reason',installed:'.spec.installed',name:'.spec.clusterName')
+  agentclusterinstall_readyforinstallation=$(oc get agentclusterinstall -A --no-headers -o custom-columns=READY:'.status.conditions[?(@.type=="Completed")].reason',name:'.metadata.name')
+  clusterdeployments_installed=$(oc get clusterdeployment -A --no-headers -o custom-columns=installed:'.spec.installed',name:'.spec.clusterName')
 
-  initialized=$(echo "$clusterdeployments_readyforinstallation_and_installed" | grep -c sno | tr -d " ")
-  booted=$(oc get bmh -A --no-headers | grep -c provisioned | tr -d " ")
+  initialized=$(echo "$clusterdeployments_installed" | grep -c sno | tr -d " ")
+
+  iso_gen=$(oc get infraenv -A -o custom-columns=NAME:.metadata.name,ISO:.status.isoDownloadURL --no-headers | grep assisted-install  | wc -l)
+  bmh_base64=$(oc get bmh -A --no-headers -o custom-columns=NAME:.metadata.name,IMAGE:.spec.image,PROVISIONED:.status.provisioning.state | base64 -w 0)
+  iso_bmh=$(echo $bmh_base64 | base64 -d | grep assisted-install | wc -l )
+  booted=$(echo $bmh_base64 | base64 -d | grep -c provisioned | tr -d " ")
   discovered=$(oc get agent -A --no-headers | wc -l | tr -d " ")
 
-  provisioning=$(echo "$clusterdeployments_readyforinstallation_and_installed" | grep -c ClusterAlreadyInstalling | tr -d " ")
-  completed=$(echo "$clusterdeployments_readyforinstallation_and_installed" | grep -i -c true | tr -d " ")
+  provisioning=$(echo "$agentclusterinstall_readyforinstallation" | grep -c InstallationInProgress | tr -d " ")
+  install_failed=$(echo "$agentclusterinstall_readyforinstallation" | grep -c InstallationFailed | tr -d " ")
+  completed=$(echo "$clusterdeployments_installed" | grep -i -c true | tr -d " ")
 
   managed=$(oc get managedcluster -A --no-headers -o custom-columns=JOINED:'.status.conditions[?(@.type=="ManagedClusterJoined")].status',AVAILABLE:'.status.conditions[?(@.type=="ManagedClusterConditionAvailable")].status' | grep -v none | grep -i true | grep -v Unknown | wc -l | tr -d " ")
   agents_available=$(oc get managedclusteraddon -A --no-headers -o custom-columns=AVAILABLE:'.status.conditions[?(@.type=="Available")].status',CLUSTER:'.status.addOnConfiguration.crName' | grep -i true | grep -c sno)
 
   echo "$D"
   echo "$D initialized: $initialized"
+  echo "$D iso_gen: $iso_gen"
+  echo "$D iso_bmh: $iso_bmh"
   echo "$D booted: $booted"
   echo "$D discovered: $discovered"
   echo "$D provisioning: $provisioning"
+  echo "$D install_failed: $install_failed"
   echo "$D completed: $completed"
   echo "$D managed: $managed"
   echo "$D agents_available: $agents_available"
@@ -69,9 +84,12 @@ while true; do
   echo "\
 $D,\
 $initialized,\
+$iso_gen,\
+$iso_bmh,\
 $booted,\
 $discovered,\
 $provisioning,\
+$install_failed,\
 $completed,\
 $managed,\
 $agents_available\
